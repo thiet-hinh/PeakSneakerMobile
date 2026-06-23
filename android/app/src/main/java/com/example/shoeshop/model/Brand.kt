355ef6ed67b5fa561733bcb69d2af7135d5c8e44
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2026-06-24 00:52:09 +0700

    New: thêm model brand, product

---
+++ android/app/src/main/java/com/example/shoeshop/model/Brand.kt
@@ -0,0 +1,6 @@
+package com.example.shoeshop.model
+data class Brand(
+    val id: Int,
+    val name: String,
+    val imageResId: Int // Dùng int tạm để load ảnh từ thư mục drawable
+)
---
+++ android/app/src/main/java/com/example/shoeshop/model/Product.kt
@@ -0,0 +1,11 @@
+package com.example.shoeshop.model
+
+data class Product(
+    val id: Int,
+    val brandName: String,
+    val name: String,
+    val rating: String,
+    val sold: String,
+    val price: String,
+    val imageResId: Int
+)
