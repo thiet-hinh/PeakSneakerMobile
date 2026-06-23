bee5d300f6f9e7f657a4d89e3bb8a633b80f109b
VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2026-06-24 00:55:28 +0700

    Fix imageResId declaration in Brand data class

--- android/app/src/main/java/com/example/shoeshop/model/Brand.kt
+++ android/app/src/main/java/com/example/shoeshop/model/Brand.kt
@@ -2,5 +2,4 @@ package com.example.shoeshop.model
 data class Brand(
     val id: Int,
     val name: String,
-    val imageResId: Int // Dùng int tạm để load ảnh từ thư mục drawable
-)
+    val imageResId: Int
